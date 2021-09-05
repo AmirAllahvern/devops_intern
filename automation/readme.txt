e472dce672c57777577c73f39bed9621de4a71be
Amir Allahvern <amir79allahvern@gmail.com>
2021-09-05 19:48:33 +0430

    automation updated

---
+++ automation/readme.txt
@@ -0,0 +1,8 @@
+DockerInstall.sh ==> bash to intall docker ce
+
+MySqlConfig.sh ==> bash to create database,table,user,and insert data
+usage: ./MySqlConfig.sh [db name] [username] [user host] [password]
+
+firewall.sh ==> enable ufw and open ports for ssh,mysql,and redis ports
+
+run.sh ==> connect to vpn and then ssh to VMs in new terminal tab
--- automation/run.sh
+++ automation/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo_pass="amir4141"
+sudo_pass="****"
 
-vm1="172.28.1.101"
-vm2="172.28.1.102"
+vm1="172.28.**.**"
+vm2="172.28.**.**"
 
-gnome-terminal --tab -- bash -c "echo $sudo_pass | sudo -S openvpn --config /home/amir/Desktop/vpn/TCP4-11955.ovpn; exec bash"
+gnome-terminal --tab -- bash -c "echo $sudo_pass | sudo -S openvpn --config /path/to/vpn/TCP4-11955.ovpn; exec bash"
 sleep 5
 gnome-terminal --tab -- bash -c "ssh root@${vm1}; exec bash"
 gnome-terminal --tab -- bash -c "ssh root@${vm2}; exec bash"
